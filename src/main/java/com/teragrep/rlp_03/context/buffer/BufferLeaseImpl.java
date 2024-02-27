c667e14cbf6ad68dd0d1c89d0b2bb6da7900ba33
eemhu <125959687+eemhu@users.noreply.github.com>
2024-02-27 14:21:27 +0200

    add PhaserDecoratedBufferLease and in BufferLeasePool use it. In InternalOffer, add stub unless  phaser was terminated explicitly

--- src/main/java/com/teragrep/rlp_03/context/buffer/BufferLeaseImpl.java
+++ src/main/java/com/teragrep/rlp_03/context/buffer/BufferLeaseImpl.java
@@ -4,20 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.ByteBuffer;
+import java.util.concurrent.Phaser;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class BufferLeaseImpl implements BufferLease {
-    private static final Logger LOGGER = LoggerFactory.getLogger(BufferLeaseImpl.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(BufferLease.class);
     private final long id;
     private final ByteBuffer buffer;
-    private long refCount; // TODO consider using a semaphore
     private final Lock lock;
 
     public BufferLeaseImpl(long id, ByteBuffer buffer) {
         this.id = id;
         this.buffer = buffer;
-        this.refCount = 0;
         this.lock = new ReentrantLock();
     }
 
@@ -26,15 +25,12 @@ public class BufferLeaseImpl implements BufferLease {
         return id;
     }
 
+    /**
+     * Unreliable, only for debug logging
+     */
     @Override
     public long refs() {
-        lock.lock();
-        try {
-            return refCount;
-        }
-        finally {
-            lock.unlock();
-        }
+        throw new IllegalStateException("refs not supported");
     }
 
     @Override
@@ -51,55 +47,27 @@ public class BufferLeaseImpl implements BufferLease {
 
     @Override
     public void addRef() {
-        lock.lock();
-        try {
-            refCount++;
-        }
-        finally {
-            lock.unlock();
-        }
+        throw new IllegalStateException("not supported on BufferLeaseImpl");
     }
 
     @Override
     public void removeRef() {
-        lock.lock();
-        try {
+        throw new IllegalStateException("not supported on BufferLeaseImpl");
 
-            long newRefs = refCount - 1;
-            if (newRefs < 0) {
-                throw new IllegalStateException("refs must not be negative");
-            }
-
-            refCount = newRefs;
-        }
-        finally {
-            lock.unlock();
-        }
     }
 
     @Override
     public boolean isRefCountZero() {
-        lock.lock();
-        try {
-            return refCount == 0;
-        }
-        finally {
-            lock.unlock();
-        }
+        throw new IllegalStateException("not supported on BufferLeaseImpl");
     }
 
 
     @Override
     public String toString() {
-        lock.lock();
-        try {
-            return "BufferLease{" +
-                    "buffer=" + buffer +
-                    ", refCount=" + refCount +
-                    '}';
-        } finally {
-            lock.unlock();
-        }
+        return "BufferLease{" +
+                "buffer=" + buffer +
+                ", refCount=" + refs() +
+                '}';
     }
 
     @Override
@@ -110,24 +78,11 @@ public class BufferLeaseImpl implements BufferLease {
 
     @Override
     public boolean attemptRelease() {
-        lock.lock();
-        try {
-            boolean rv = false;
-            removeRef();
-            if (isRefCountZero()) {
-                buffer().clear();
-                // LOGGER.info("released bufferLease id <{}>, refs <{}>", bufferLease.id(), bufferLease.refs());
-                rv = true;
-            }
-            return rv;
-
-        } finally {
-            lock.unlock();
-        }
+        throw new IllegalStateException("not supported on BufferLeaseImpl");
     }
 
     @Override
-    public Phaser phaser() {
-        return this.phaser;
+    public boolean terminated() {
+        throw new IllegalStateException("not supported on BufferLeaseImpl");
     }
 }
